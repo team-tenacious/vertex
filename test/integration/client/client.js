33d90f5c7f1cc76e101c783942723ea6f32bb026
southbite <simon@tenacious.digital>
2018-02-12 17:44:14 +0200

    test: client disconnect

--- test/integration/client/client.js
+++ test/integration/client/client.js
@@ -33,7 +33,7 @@ describe(filename, function () {
     }
 
     close(code, reason) {
-
+      this.emit('close', {code: code, reason: reason});
     }
 
     connect(url, options) {
@@ -59,9 +59,39 @@ describe(filename, function () {
     //   console.log(event.key, event.data);
     // });
     //
-    client.on('connected', function(connection){
+    client.on('connected', function(){
       done();
       //setTimeout(done, 5000);
+    });
+
+    client.connect();
+  });
+
+  it('mocks the connection uses it to test the client disconnect', function (done) {
+
+    this.timeout(10000);
+
+    var Client = require('../../..').Client;
+
+    var config = {url:'ws://127.0.0.1:3737'};
+
+    var client = new Client(config, {
+      Connection:Connection
+    });
+
+    // client.on('all', function(event){
+    //   console.log(event.key, event.data);
+    // });
+    //
+    client.on('connected', function(){
+
+      client.on('close', function(info){
+        expect(info.code).to.be(1);
+        expect(info.reason).to.be('intentional disconnect test');
+        done();
+      });
+
+      client.disconnect({code: 1, reason: 'intentional disconnect test'});
     });
 
     client.connect();
