cff0fa57560ce7dc728fc28f86724de4a8761d7b
southbite <simon@tenacious.digital>
2018-02-16 17:19:37 +0200

    test: publish edge action unit tested

--- test/unit/services/edge.js
+++ test/unit/services/edge.js
@@ -9,41 +9,44 @@ describe(filename, function () {
 
   function mockServer() {
 
-    var ws =  new EventEmitter();
-    var hashring =  new EventEmitter();
-    var cluster =  new EventEmitter();
-    var cache =  new EventEmitter();
+    var ws = new EventEmitter();
+    var hashring = new EventEmitter();
+    var cluster = new EventEmitter();
+    var cache = new EventEmitter();
 
     var __cache = {};
 
     ws.write = (sessionId, message) => {
-      ws.emit('wrote', {sessionId:sessionId, message:message});
+      ws.emit('wrote', {sessionId: sessionId, message: message});
     };
 
-    cache.get = function(key){
-      return new Promise(function(resolve){
+    cache.get = function (key) {
+      return new Promise(function (resolve) {
         resolve(__cache[key] || []);
       });
     };
 
-    cache.set = function(key, value){
-      return new Promise(function(resolve){
+    cache.set = function (key, value) {
+      return new Promise(function (resolve) {
         __cache[key] = value;
         resolve(value);
       });
     };
 
-    cluster.listPeers = function(){
-      return ['10:0.0.1:5000','10:0.0.2:5000','10:0.0.3:5000','10:0.0.4:5000'];
+    cluster.listPeers = function () {
+      return ['10:0.0.1:5000', '10:0.0.2:5000', '10:0.0.3:5000', '10:0.0.4:5000'];
     };
 
-    cluster.request = function(peer, message){
-      return new Promise(function(resolve){
+    cluster.request = function (peer, message) {
+      return new Promise(function (resolve) {
+
+        if (message.action == 'subscription-query') return resolve({edges: ['10:0.0.1:5000', '10:0.0.2:5000']});
+
         resolve('ok');
       })
     };
 
-    hashring.listMembers = function(topic){
+    hashring.listMembers = function (topic) {
       return ['10:0.0.1:5000'];
     };
 
@@ -80,12 +83,12 @@ describe(filename, function () {
 
     var edge = new Edge(server, mockLogger(), mockConfig());
 
-    edge.on('message-process-error', function(e){
+    edge.on('message-process-error', function (e) {
       expect(e.toString()).to.be('Error: No handler for action: unknown');
       done();
     });
 
-    server.services.ws.emit('message', {sessionId:'testId', data:{action:'unknown'}});
+    server.services.ws.emit('message', {sessionId: 'testId', data: {action: 'unknown'}});
   });
 
   it('starts the edge service, pushes a subscribe action through', function (done) {
@@ -94,12 +97,15 @@ describe(filename, function () {
 
     var edge = new Edge(server, mockLogger(), mockConfig());
 
-    edge.on('message-process-ok', function(data){
+    edge.on('message-process-ok', function (data) {
       expect(data.response).to.be('ok');
       done();
     });
 
-    server.services.ws.emit('message', {sessionId:'testId', data:{action:'subscribe', payload:{topic:'test-topic'}}});
+    server.services.ws.emit('message', {
+      sessionId: 'testId',
+      data: {action: 'subscribe', payload: {topic: 'test-topic'}}
+    });
   });
 
   it('starts the edge service, pushes an unsubscribe action through', function (done) {
@@ -108,16 +114,53 @@ describe(filename, function () {
 
     var edge = new Edge(server, mockLogger(), mockConfig());
 
-    edge.on('message-process-ok', function(data){
+    edge.on('message-process-ok', function (data) {
       expect(data.response).to.be('ok');
       if (data.message.data.action == 'subscribe')
-        server.services.ws.emit('message', {sessionId:'testId', data:{action:'unsubscribe', payload:{topic:'test-topic'}}});
+        server.services.ws.emit('message', {
+          sessionId: 'testId',
+          data: {action: 'unsubscribe', payload: {topic: 'test-topic'}}
+        });
       else {
         expect(data.message.data.action).to.be('unsubscribe');
         done();
       }
     });
 
-    server.services.ws.emit('message', {sessionId:'testId', data:{action:'subscribe', payload:{topic:'test-topic'}}});
+    server.services.ws.emit('message', {
+      sessionId: 'testId',
+      data: {action: 'subscribe', payload: {topic: 'test-topic'}}
+    });
+  });
+
+  it('starts the edge service, pushes a publish action through', function (done) {
+
+    var server = mockServer();
+
+    var edge = new Edge(server, mockLogger(), mockConfig());
+
+    edge.on('message-process-ok', function (data) {
+
+      if (data.message.data.action == 'subscribe') {
+
+        expect(data.response).to.be('ok');
+
+        server.services.ws.emit('message', {
+          sessionId: 'testId',
+          data: {action: 'publish', payload: {topic: 'test-topic', data: 'test-data'}}
+        });
+      }
+      else {
+
+        expect(data.message.data.action).to.be('publish');
+        expect(data.response).to.eql({'test-topic': 2, '*': 2});
+        done();
+      }
+    });
+
+    server.services.ws.emit('message', {
+      sessionId: 'testId',
+      data: {action: 'subscribe', payload: {topic: 'test-topic'}}
+    });
   });
 });
